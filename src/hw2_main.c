6fdb5c1d575768a9097bceb31e83ae10682f8149
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-23 15:10:33 +0000

    Update

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -13,85 +13,109 @@ int main(int argc, char **argv) {
     int c = 0;
     int iflag = 0, oflag = 0, cflag = 0, pflag = 0, rflag = 0;
     FILE *input_file;
+    FILE *output_file;
+    FILE *font_file;
+    int c_param_len = 0, p_param_len = 0, r_param_len = 0;
 
     while ((c = getopt(argc, argv, "i:o:c:p:r:")) != -1) {
         switch(c) {
             case 'i':
                 if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
-                    printf("Missing Argument_i");
                     return MISSING_ARGUMENT;
                 }
                 if (iflag) {
-                    printf("Duplicate Arguement_i");
                     return DUPLICATE_ARGUMENT;
                 }
-
                 iflag = 1;
                 input_file = fopen(optarg, "r");
-
                 break;
             case 'o':
                 if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
-                    printf("Missing Argument_i");
                     return MISSING_ARGUMENT;
                 }
                 if (oflag) {
-                    printf("Duplicate Arguement_o");
                     return DUPLICATE_ARGUMENT;
                 }
-
                 oflag = 1;
+                output_file = fopen(optarg, "w");
                 break;
             case 'c':
                 if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
-                    printf("Missing Argument_i");
                     return MISSING_ARGUMENT;
                 }
                 if (cflag) {
-                    printf("Duplicate Arguement_c");
                     return DUPLICATE_ARGUMENT;
                 }
-
                 cflag = 1;
+                
+                char* token_c = strtok(optarg, ",");
+                while (token_c) {
+                    c_param_len += 1;
+                    token_c = strtok(NULL, ",");
+                }
+
                 break;
             case 'p':
                 if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
-                    printf("Missing Argument_i");
                     return MISSING_ARGUMENT;
                 }
                 if (pflag) {
-                    printf("Duplicate Arguement_p");
                     return DUPLICATE_ARGUMENT;
                 }
-
                 pflag = 1;
+
+                char* token_p = strtok(optarg, ",");
+                while (token_p) {
+                    p_param_len += 1;
+                    token_p = strtok(NULL, ",");
+                }
+
                 break;
             case 'r':
                 if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
-                    printf("Missing Argument_i");
                     return MISSING_ARGUMENT;
                 }
                 if (rflag) {
-                    printf("Duplicate Arguement_r");
                     return DUPLICATE_ARGUMENT;
                 }
-
                 rflag = 1;
+
+                char* token_r = strtok(optarg, ",");
+                while (token_r) {
+                    r_param_len += 1;
+                    token_r = strtok(NULL, ",");
+
+                    if (r_param_len == 2) {
+                        font_file = fopen(token_r, "r");
+                    }
+                }
+
                 break;
             case '?':
-                printf("Unrecognized Arguemnt");
                 return UNRECOGNIZED_ARGUMENT;
         }
     }
 
     if (!oflag || !iflag) {
-        printf("missing argument");
         return MISSING_ARGUMENT;
     }
-
     if (input_file == NULL) {
         return INPUT_FILE_MISSING;
     }
-
+    if (output_file == NULL) {
+        return OUTPUT_FILE_UNWRITABLE;
+    }
+    if (pflag && !cflag) {
+        return C_ARGUMENT_MISSING;
+    }
+    if (cflag && c_param_len != 4) {
+        return C_ARGUMENT_INVALID;
+    }
+    if (pflag && p_param_len != 2) {
+        return P_ARGUMENT_INVALID;
+    }
+    if (rflag && (font_file == NULL || r_param_len != 5)) {
+        return R_ARGUMENT_INVALID;
+    }
     return 0;
 }
