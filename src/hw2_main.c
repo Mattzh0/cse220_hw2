8bc0a93232366498614fe9c61fefe1d8e5994831
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-23 05:54:56 +0000

    Update

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -11,24 +11,87 @@
 
 int main(int argc, char **argv) {
     int c = 0;
-    int o_count = 0;
-    int i_count = 0;
+    int iflag = 0, oflag = 0, cflag = 0, pflag = 0, rflag = 0;
+    FILE *input_file;
 
-    while ((c = getopt(argc, argv, "o:i:")) != -1) {
+    while ((c = getopt(argc, argv, "i:o:c:p:r:")) != -1) {
         switch(c) {
-            case 'o':
-                o_count += 1;
-                break;
             case 'i':
-                i_count += 1;
+                if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
+                    printf("Missing Argument_i");
+                    return MISSING_ARGUMENT;
+                }
+                if (iflag) {
+                    printf("Duplicate Arguement_i");
+                    return DUPLICATE_ARGUMENT;
+                }
+
+                iflag = 1;
+                input_file = fopen(optarg, "r");
+
                 break;
+            case 'o':
+                if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
+                    printf("Missing Argument_i");
+                    return MISSING_ARGUMENT;
+                }
+                if (oflag) {
+                    printf("Duplicate Arguement_o");
+                    return DUPLICATE_ARGUMENT;
+                }
+
+                oflag = 1;
+                break;
+            case 'c':
+                if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
+                    printf("Missing Argument_i");
+                    return MISSING_ARGUMENT;
+                }
+                if (cflag) {
+                    printf("Duplicate Arguement_c");
+                    return DUPLICATE_ARGUMENT;
+                }
+
+                cflag = 1;
+                break;
+            case 'p':
+                if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
+                    printf("Missing Argument_i");
+                    return MISSING_ARGUMENT;
+                }
+                if (pflag) {
+                    printf("Duplicate Arguement_p");
+                    return DUPLICATE_ARGUMENT;
+                }
+
+                pflag = 1;
+                break;
+            case 'r':
+                if (optind + 1 == argc || optarg == NULL || (optarg && optarg[0] == '-')) {
+                    printf("Missing Argument_i");
+                    return MISSING_ARGUMENT;
+                }
+                if (rflag) {
+                    printf("Duplicate Arguement_r");
+                    return DUPLICATE_ARGUMENT;
+                }
+
+                rflag = 1;
+                break;
+            case '?':
+                printf("Unrecognized Arguemnt");
+                return UNRECOGNIZED_ARGUMENT;
         }
     }
 
-    if (o_count == 1 && i_count == 1) {
-        return 0;
-    }
-    else {
+    if (!oflag || !iflag) {
+        printf("missing argument");
         return MISSING_ARGUMENT;
     }
+
+    if (input_file == NULL) {
+        return INPUT_FILE_MISSING;
+    }
+
+    return 0;
 }
