c8c00d756930325993c6c626ad79679236c22012
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-23 00:24:52 +0000

    Update

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -10,8 +10,23 @@
 #include <unistd.h> 
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    int i;
+    int o_count = 0;
+    int i_count = 0;
 
-    return 1000;
+    for (i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-o") == 0 && i+1 < argc && argv[i][0] != '-') {
+            o_count += 1;
+        }
+        else if (strcmp(argv[i], "-i") == 0 && i+1 < argc && argv[i][0] != '-') {
+            i_count += 1;
+        }
+    }
+
+    if (o_count == 1 && i_count == 1) {
+        return 0;
+    }
+    else {
+        return MISSING_ARGUMENT;
+    }
 }
